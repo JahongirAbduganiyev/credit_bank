6534f66ea382efff70e617b01dee9476e6ec1f1f
Jonny <jahongirabduganiyev7@gmail.com>
2021-09-03 16:49:30 +0500

    prasrochka bolimi qoshildi

--- pages/prasrochka.php
+++ pages/prasrochka.php
@@ -1,1 +1,68 @@
-<?php
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Prasrochka</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Bosh sahifa</a></li>
+                        <li class="breadcrumb-item active">Prasrochka</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">To'lov muddati o'tgan xaridorlar</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped text-center">
+                                <thead>
+                                <tr>
+                                    <th>Client ID</th>
+                                    <th>Credit ID</th>
+                                    <th>FISH</th>
+                                    <th>Qarzdorlik</th>
+                                    <th>Telefon</th>
+                                    <th>Status</th>
+                                    <th style="width: 100px;">Sozlash</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th>72890</th>
+                                        <th>02</th>
+                                        <th>Abduganiyev Jahongir</th>
+                                        <th>120 000</th>
+                                        <th>993153755</th>
+                                        <th>0.1</th>
+                                        <th></th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
